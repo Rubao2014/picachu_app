cf007b5e3adfd05b3ed2c97d15625375392b00b7
Rubens Souza <r_henrique@yahoo.com>
2016-10-22 16:25:33 -0200

    PG for PROD

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20161015193111) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cadastros", force: :cascade do |t|
     t.string   "nome"
     t.string   "nasc"
